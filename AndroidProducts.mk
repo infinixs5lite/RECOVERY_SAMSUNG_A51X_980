29843b5dc326f9480685e6c9dc583a5a5538521a
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-29 22:42:13 +0100

    fixing build errors

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a51x.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a51x-user \
+    twrp_a51x-userdebug \
+    twrp_a51x-eng
