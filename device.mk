0e1a4a1eb6ca072f3f92a4a94e7a390130d8286a
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-31 16:59:20 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,7 +21,3 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
-# Screen
-TARGET_SCREEN_WIDTH := 1440
-TARGET_SCREEN_HEIGHT := 3088
