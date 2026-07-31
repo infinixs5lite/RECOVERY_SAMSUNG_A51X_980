4f527316764d527eac36a4d9a0a763367a639678
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-07-31 07:42:29 +0100

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -1,6 +1,3 @@
-MKBOOTIMG := device/samsung/a51x/mkbootimg
-MKBOOTIMG := $(DEVICE_PATH)/mkbootimg
-
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$(1)) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
