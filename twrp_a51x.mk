bbcf792f6fdc34b308df5a2f1498bb0581e5a974
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-08-05 19:47:37 +0100

    Update twrp_a51x.mk

--- twrp_a51x.mk
+++ twrp_a51x.mk
@@ -1,5 +1,6 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,29 +14,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+DEVICE_PATH := device/samsung/a51x
 
 # Release name
 PRODUCT_RELEASE_NAME := a51x
 
 # Inherit from common AOSP config
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/a51x/device.mk)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Manual workaround for common tree
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
+- PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
++ PRODUCT_COPY_FILES += $(call cc_prebuilt_binary, sgdisk)
++ PRODUCT_COPY_FILES += $(call cc_prebuilt_binary, gdisk)
++ PRODUCT_COPY_FILES += $(call cc_prebuilt_binary, fixparts)
++ PRODUCT_COPY_FILES += $(call cc_prebuilt_binary, lz4.bin)
 
 PRODUCT_DEVICE := a51x
 PRODUCT_NAME := twrp_a51x
